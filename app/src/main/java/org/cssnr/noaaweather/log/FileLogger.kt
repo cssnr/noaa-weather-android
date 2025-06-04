4a057d91169e491aa3c7f27153782d470ae485b6
Shane <6071159+smashedr@users.noreply.github.com>
2025-06-04 02:13:12 -0700

    Version 2

--- app/src/main/java/org/cssnr/noaaweather/log/FileLogger.kt
+++ app/src/main/java/org/cssnr/noaaweather/log/FileLogger.kt
@@ -3,6 +3,9 @@ package org.cssnr.noaaweather.log
 import android.content.Context
 import android.util.Log
 import androidx.preference.PreferenceManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
@@ -13,23 +16,28 @@ import java.util.Locale
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
+private val logLock = ReentrantLock()
+
 fun Context.debugLog(message: String) {
-    val debugLog =
-        PreferenceManager.getDefaultSharedPreferences(this).getBoolean("enable_debug_logs", false)
+    val debugLog = PreferenceManager.getDefaultSharedPreferences(this)
+        .getBoolean("enable_debug_logs", false)
     if (!debugLog) return
-    val lock = ReentrantLock()
+
     val logFile = File(filesDir, "debug_log.txt")
-    val logWriter = BufferedWriter(FileWriter(logFile, true))
     val timeStamp = SimpleDateFormat("MM-dd HH:mm:ss", Locale.US).format(Date())
-    val logMessage = "$timeStamp - ${message}\n"
-    Log.d("appendLog", "logMessage: $logMessage")
-    lock.withLock {
-        try {
-            logWriter.write(logMessage)
-            logWriter.flush()
-            logWriter.close()
-        } catch (e: IOException) {
-            Log.e("appendLog", "IOException: $e")
+    val logMessage = "$timeStamp - $message\n"
+
+    CoroutineScope(Dispatchers.IO).launch {
+        logLock.withLock {
+            try {
+                FileWriter(logFile, true).use { fw ->
+                    BufferedWriter(fw).use { bw ->
+                        bw.write(logMessage)
+                    }
+                }
+            } catch (e: IOException) {
+                Log.e("debugLog", "IOException: $e")
+            }
         }
     }
 }
