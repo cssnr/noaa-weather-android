9833e46bcda868924a751221d1f4fa246364d739
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-08-24 23:49:49 -0700

    Add ACRA (#51)

--- app/src/main/java/org/cssnr/noaaweather/ui/settings/SettingsFragment.kt
+++ app/src/main/java/org/cssnr/noaaweather/ui/settings/SettingsFragment.kt
@@ -12,8 +12,6 @@ import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
 import android.provider.Settings
-import android.text.Html
-import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.EditText
